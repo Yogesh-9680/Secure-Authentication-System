8cc63057b60eb27107387074c0013ec3e19fa4ac
Yogesh-9680 <yogeshgupta9352@gmail.com>
2026-07-03 19:54:37 +0530

    Add files via upload

---
+++ secure authentication system.py
@@ -0,0 +1,77 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/yoges/OneDrive/Attachments/Documents/Downloads/secure authentication system.py
+
+==== Secure Authentication System ====
+1. Register
+2. Login
+3. Exit
+
+Choice: 1
+
+=== Register ===
+New username: h
+New password: 2
+Registration successful.
+
+==== Secure Authentication System ====
+1. Register
+2. Login
+3. Exit
+
+Choice: 2
+
+=== Login ===
+Username: 1
+Password: 0
+Invalid credentials.
+Failed attempts: 1
+
+==== Secure Authentication System ====
+1. Register
+2. Login
+3. Exit
+
+Choice: 2
+
+=== Login ===
+Username: h
+Password: 2
+Login successful!
+
+=== Welcome h ===
+1. Change Password
+2. OTP Verification Demo
+3. Logout
+
+Choice: 1
+Old password: 2
+New password: 3
+Password updated successfully.
+
+=== Welcome h ===
+1. Change Password
+2. OTP Verification Demo
+3. Logout
+
+Choice: 2
+Generated OTP (demo): 464779
+Enter OTP: 464778
+Invalid OTP.
+
+=== Welcome h ===
+1. Change Password
+2. OTP Verification Demo
+3. Logout
+
+Choice: 3
+Logged out.
+
+==== Secure Authentication System ====
+1. Register
+2. Login
+3. Exit
+
+Choice: 3
+Goodbye!
